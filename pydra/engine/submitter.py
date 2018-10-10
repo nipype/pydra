1f1fbacca595a6b5ed25939d38b8e9b5f4479180
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-10-09 21:20:29 -0400

    MNT: Drop Python 2 vestiges

--- pydra/engine/submitter.py
+++ pydra/engine/submitter.py
@@ -1,9 +1,3 @@
-from __future__ import print_function, division, unicode_literals, absolute_import
-from builtins import object
-
-from future import standard_library
-standard_library.install_aliases()
-
 import os, pdb, time
 from copy import deepcopy
 
