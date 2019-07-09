c51c059e81ee7fc08ff6759ace8edf390c0f33b7
Dorota Jarecka <djarecka@gmail.com>
2019-07-09 16:26:31 -0400

    adding comments

--- pydra/engine/graph.py
+++ pydra/engine/graph.py
@@ -20,6 +20,7 @@ class Graph:
 
     @nodes.setter
     def nodes(self, nodes):
+        """ returns a list of all nodes or an empty list"""
         if nodes:
             nodes = ensure_list(nodes)
             if len(set(nodes)) != len(nodes):
@@ -30,6 +31,7 @@ class Graph:
 
     @property
     def nodes_names_map(self):
+        """mapping node names to objects"""
         return {nd.name: nd for nd in self.nodes}
 
     @property
@@ -38,6 +40,7 @@ class Graph:
 
     @edges.setter
     def edges(self, edges):
+        """adding edges to the graph (nodes has to be already in self.nodes)"""
         if edges:
             edges = ensure_list(edges)
             for (nd_out, nd_in) in edges:
@@ -51,19 +54,23 @@ class Graph:
 
     @property
     def edges_names(self):
+        """returns edges using the nodes names"""
         return [(edg[0].name, edg[1].name) for edg in self._edges]
 
     @property
     def sorted_nodes(self):
+        """ returns sorted nodes (runs sorting if needed)"""
         if self._sorted_nodes is None:
             self.sorting()
         return self._sorted_nodes
 
     @property
     def sorted_nodes_names(self):
+        """returns sorted nodes names"""
         return [nd.name for nd in self._sorted_nodes]
 
     def _create_connections(self):
+        """creates connections between nodes"""
         self.connections_pred = {}
         self.connections_succ = {}
         for nd in self.nodes:
@@ -75,7 +82,7 @@ class Graph:
             self.connections_succ[nd_out.name].append(nd_in)
 
     def add_nodes(self, new_nodes):
-        # todo?
+        """adding new nodes and sorting the graph"""
         self.nodes = self._nodes + ensure_list(new_nodes)
         for nd in ensure_list(new_nodes):
             self.connections_pred[nd.name] = []
@@ -85,27 +92,17 @@ class Graph:
             self.sorting(presorted=self.sorted_nodes + ensure_list(new_nodes))
 
     def add_edges(self, new_edges):
-        # todo?
+        """ adding new edges and sorting the graph"""
         self.edges = self._edges + ensure_list(new_edges)
         for (nd_out, nd_in) in ensure_list(new_edges):
             self.connections_pred[nd_in.name].append(nd_out)
             self.connections_succ[nd_out.name].append(nd_in)
         if self._sorted_nodes is not None:
-            # starting from the previous sorted list, so is faster
+            # starting from the previous sorted list, so it's faster
             self.sorting(presorted=self.sorted_nodes + [])
 
-    def _sorting(self, notsorted_list, connections_pred):
-        left_nodes = []
-        sorted_part = []
-        for nd in notsorted_list:
-            if not connections_pred[nd.name]:
-                sorted_part.append(nd)
-            else:
-                left_nodes.append(nd)
-        return sorted_part, left_nodes
-
     def sorting(self, presorted=None):
-        """sorting the graph"""
+        """sorting the graph, starting either from self.nodes or the previously sorted list"""
         self._sorted_nodes = []
         if presorted:
             notsorted_nodes = copy(presorted)
@@ -123,6 +120,19 @@ class Graph:
             for nd_out in sorted_part:
                 for nd_in in self.connections_succ[nd_out.name]:
                     connections_pred[nd_in.name].remove(nd_out)
+
+    def _sorting(self, notsorted_list, connections_pred):
+        """ adding nodes that don't have predecessors to the sorted_parts,
+            returns sorted part and remaining nodes
+        """
+        remaining_nodes = []
+        sorted_part = []
+        for nd in notsorted_list:
+            if not connections_pred[nd.name]:
+                sorted_part.append(nd)
+            else:
+                remaining_nodes.append(nd)
+        return sorted_part, remaining_nodes
 
     def remove_node(self, node):
         """removing a node, re-sorting if needed"""
@@ -165,7 +175,7 @@ class Graph:
             )
 
     def calculate_max_paths(self):
-        """ calculate maximum paths between any node without in connections (no predecessors)
+        """ calculate maximum paths between any node without "history" (no predecessors)
         and all of the connections
         """
         self.max_paths = {}
