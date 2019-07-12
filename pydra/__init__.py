64e822bb7a5742087d47b4dcf300af15b6fe1fa8
mathiasg <mathiasg@mit.edu>
2019-07-12 16:40:59 -0400

    ignore import failures on setup

--- pydra/__init__.py
+++ pydra/__init__.py
@@ -1,4 +1,20 @@
+from .__about__ import (
+    __version__,
+    __author__,
+    __license__,
+    __maintainer__,
+    __email__,
+    __status__,
+    __url__,
+    __packagename__,
+    __description__,
+    __longdesc__,
+)
+
 # expose api
-from .engine.core import Workflow
-from .engine.task import to_task
-from .engine.submitter import Submitter
+try:
+    from .engine.core import Workflow
+    from .engine.task import to_task
+    from .engine.submitter import Submitter
+except ImportError:  # on install
+    pass
