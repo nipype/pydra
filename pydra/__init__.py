401049d12aa3f95ece638b14ee97e57fe43ec47d
mathiasg <mathiasg@mit.edu>
2019-07-12 16:33:55 -0400

    rf: isolate information to __about__

--- pydra/__init__.py
+++ pydra/__init__.py
@@ -1,16 +1,3 @@
-from .__about__ import (
-    __version__,
-    __author__,
-    __license__,
-    __maintainer__,
-    __email__,
-    __status__,
-    __url__,
-    __packagename__,
-    __description__,
-    __longdesc__,
-)
-
 # expose api
 from .engine.core import Workflow
 from .engine.task import to_task
