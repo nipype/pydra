37d96c7ccc857bd0e9aa01517da8cf53c1242947
mathiasg <mathiasg@mit.edu>
2019-07-23 18:18:20 -0400

    fix: allow ports to open + assign instance name

--- ci/slurm.sh
+++ ci/slurm.sh
@@ -5,8 +5,9 @@ export DOCKER_IMAGE="mgxd/slurm:19.05.1"
 function travis_before_install {
     docker pull ${DOCKER_IMAGE}
     # have image running in background
-    export CONTAINER_ID=$(docker run -itd -h ernie -v `pwd`:/pydra ${DOCKER_IMAGE})
-    docker exec ${CONTAINER_ID} bash -c "sacct && sinfo && squeue" 2&> /dev/null
+    docker run -itd -h ernie --name slurm -v `pwd`:/pydra ${DOCKER_IMAGE}
+    echo "Sleeping so ports can wake up" && sleep 10
+    docker exec slurm bash -c "sacct && sinfo && squeue" 2&> /dev/null
     if [ $? -ne 0 ]; then
         echo "Slurm docker image error"
         exit 1
@@ -14,7 +15,7 @@ function travis_before_install {
 }
 
 function travis_install {
-    docker exec ${CONTAINER_ID} bash -c "cd /pydra && pip install -e .[test] && python -c 'import pydra; print(pydra.__version__)'"
+    docker exec slurm bash -c "cd /pydra && pip install -e .[test] && python -c 'import pydra; print(pydra.__version__)'"
 }
 
 function travis_before_script {
@@ -22,11 +23,10 @@ function travis_before_script {
 }
 
 function travis_script {
-    docker exec ${CONTAINER_ID} bash -c "pytest -vs -n auto --cov pydra --cov-config .coveragerc --cov-report xml:cov.xml --doctest-modules pydra/engine/"
+    docker exec slurm bash -c "pytest -vs -n auto --cov pydra --cov-config .coveragerc --cov-report xml:cov.xml --doctest-modules pydra/engine/"
 }
 
 function travis_after_script {
-    docker exec ${CONTAINER_ID} bash -c "codecov --file cov.xml --flags unittests"
-    docker stop ${CONTAINER_ID}
-    unset ${CONTAINER_ID}
+    docker exec slurm bash -c "codecov --file cov.xml --flags unittests"
+    docker stop slurm && docker rm slurm
 }
