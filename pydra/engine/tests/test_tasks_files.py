6019e16d3fb084e766351e0f53857a1de7387975
Dorota Jarecka <djarecka@gmail.com>
2019-07-26 16:56:20 -0400

    two simple tests for task and wf that take file as an input; works with absolut paths only

--- pydra/engine/tests/test_tasks_files.py
+++ pydra/engine/tests/test_tasks_files.py
@@ -1,8 +1,10 @@
-import os, shutil
+import os
+from pathlib import Path
 import numpy as np
 import pytest
 
 from ..submitter import Submitter
+from ..core import Workflow
 from ..task import to_task
 
 
@@ -13,59 +15,58 @@ def file_add2(file):
     cwd = os.getcwd()
     # providing a full path
     file_out = os.path.join(cwd, "arr_out.npy")
-    # providing relative path
-    file_out_default = os.path.join("arr_out.npy")
     np.save(file_out, array_out)
-    return file_out, file_out_default
+    return file_out
 
 
 @to_task
-def file_mult(file1, file2):
-    array_inp1 = np.load(file1)
-    array_inp2 = np.load(file2)
-    array_out = array_inp1 * array_inp2
+def file_mult(file):
+    array_inp = np.load(file)
+    array_out = 10 * array_inp
     cwd = os.getcwd()
     file_out = os.path.join(cwd, "arr_out.npy")
     np.save(file_out, array_out)
     return file_out
 
 
-def test_1(tmpdir):
-    """ using full path in the function"""
+def test_task_1(tmpdir):
+    """ task that takes file as an input"""
     os.chdir(tmpdir)
     arr = np.array([2])
+    # creating abs path
     file = os.path.join(os.getcwd(), "arr1.npy")
     np.save(file, arr)
     nn = file_add2(name="add2", file=file)
-    # assert np.allclose(nn.inputs.a, [3])
-    assert nn.state is None
 
     with Submitter(plugin="cf") as sub:
         sub(nn)
 
     # checking the results
     results = nn.result()
-
-    res = np.load(results.output.out[0])
+    res = np.load(results.output.out)
     assert res == np.array([4])
 
 
-@pytest.mark.xfail(reason="doesn't work with relative path (can't find the file), TODO")
-def test_default_out_1(tmpdir):
-    """ using relative path in the function"""
+def test_wf_1(tmpdir):
+    """ workflow with 2 tasks that take file as an input and give file as an aoutput"""
+    wf = Workflow(name="wf_1", input_spec=["file_orig"])
+    wf.add(file_add2(name="add2", file=wf.lzin.file_orig))
+    wf.add(file_mult(name="mult", file=wf.add2.lzout.out))
+    wf.set_output([("out", wf.mult.lzout.out)])
+
     os.chdir(tmpdir)
-    arr = np.array([2])
-    file = os.path.join(os.getcwd(), "arr1.npy")
-    np.save(file, arr)
-    nn = file_add2(name="add2", file=file)
-    # assert np.allclose(nn.inputs.a, [3])
-    assert nn.state is None
+    arr = np.array([2, 3])
+    # creating abs path
+    file_orig = os.path.join(os.getcwd(), "arr_orig.npy")
+    np.save(file_orig, arr)
+    wf.inputs.file_orig = file_orig
 
     with Submitter(plugin="cf") as sub:
-        sub(nn)
+        sub(wf)
 
-    # checking the results
-    results = nn.result()
-
-    res = np.load(results.output.out[1])
-    assert res == np.array([4])
+    assert wf.output_dir.exists()
+    file_output = wf.result().output.out
+    assert Path(file_output).exists()
+    # loading results
+    array_out = np.load(file_output)
+    assert np.array_equal(array_out, [40, 50])
