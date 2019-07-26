6833a80e4e55b92841f0d3ee189a070e45d17a15
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-07-26 11:39:33 -0400

    ENH: Add pydra.mark module with initial annotate and task decorators

---
+++ pydra/mark/tests/test_functions.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import random
+
+from ..functions import task, annotate
+from ...engine.task import FunctionTask
+
+
+def test_task_equivalence():
+    def add_two(a):
+        return a + 2
+
+    canonical = FunctionTask(add_two, a=3)
+
+    decorated1 = task(add_two)(a=3)
+
+    @task
+    def addtwo(a):
+        return a + 2
+
+    decorated2 = addtwo(a=3)
+
+    assert canonical.checksum == decorated1.checksum
+
+    c_res = canonical._run()
+    d1_res = decorated1._run()
+    d2_res = decorated2._run()
+
+    assert c_res.output.hash == d1_res.output.hash
+    assert c_res.output.hash == d2_res.output.hash
+
+
+def test_annotation_equivalence():
+    def direct(a: int) -> int:
+        return a + 2
+
+    @annotate({"return": int})
+    def partial(a: int):
+        return a + 2
+
+    @annotate({"a": int, "return": int})
+    def indirect(a):
+        return a + 2
+
+    assert direct.__annotations__ == partial.__annotations__
+    assert direct.__annotations__ == indirect.__annotations__
+
+    # Run functions to ensure behavior is unaffected
+    a = random.randint(0, (1 << 32) - 3)
+    assert direct(a) == partial(a)
+    assert direct(a) == indirect(a)
+
+
+def test_annotation_override():
+    @annotate({"a": float, "return": float})
+    def annotated(a: int) -> int:
+        return a + 2
+
+    assert annotated.__annotations__ == {"a": float, "return": float}
+
+
+def test_invalid_annotation():
+    with pytest.raises(TypeError):
+
+        @annotate({"b": int})
+        def addtwo(a):
+            return a + 2
