457a073571f405d9172415a7b260a57927130bd2
Dorota Jarecka <djarecka@gmail.com>
2019-07-29 11:48:11 -0400

    adding classes to __init__ files

--- pydra/__init__.py
+++ pydra/__init__.py
@@ -3,4 +3,5 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-from .engine import to_task, Submitter, Workflow
+from .engine import Submitter, Workflow, AuditFlag
+from . import mark
--- pydra/engine/__init__.py
+++ pydra/engine/__init__.py
@@ -1,3 +1,3 @@
-from .task import to_task
 from .submitter import Submitter
 from .core import Workflow
+from .task import AuditFlag
