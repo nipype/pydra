c646892c5685a5f4ebaa2bc6aee4e9544a62a5f1
Dorota Jarecka <djarecka@gmail.com>
2020-05-14 17:20:42 -0400

    adding skipif to the numpy test, removing cache_dir from the test

--- pydra/engine/tests/test_numpy_examples.py
+++ pydra/engine/tests/test_numpy_examples.py
@@ -1,11 +1,14 @@
 import numpy as np
 import typing as ty
-import os
+import importlib
 import pytest
 
 from ..submitter import Submitter
 from ..core import Workflow
 from ...mark import task, annotate
+
+if importlib.util.find_spec("numpy") is None:
+    pytest.skip("can't find numpy library", allow_module_level=True)
 
 
 @task
@@ -16,12 +19,11 @@ def arrayout(val):
 
 def test_multiout(plugin):
     """ testing a simple function that returns a numpy array"""
-    cache_dir = os.path.join(os.getcwd(), "cache3")
-    wf = Workflow("wf", input_spec=["val"], val=[0, 1, 2], cache_dir=cache_dir)
-    wf.add(arrayout(name="mo", val=wf.lzin.val, cache_dir=cache_dir))
+    wf = Workflow("wf", input_spec=["val"], val=[0, 1, 2])
+    wf.add(arrayout(name="mo", val=wf.lzin.val))
     wf.mo.split("val").combine("val")
 
-    wf.set_output([("b", wf.mo.lzout.b)])
+    wf.set_output([("array", wf.mo.lzout.b)])
 
     with Submitter(plugin=plugin, n_procs=2) as sub:
         sub(runnable=wf)
@@ -30,4 +32,4 @@ def test_multiout(plugin):
 
     assert results[0] == {"wf.val": [0, 1, 2]}
     for el in range(3):
-        assert np.array_equal(results[1].output.b[el], np.array([el, el]))
+        assert np.array_equal(results[1].output.array[el], np.array([el, el]))
