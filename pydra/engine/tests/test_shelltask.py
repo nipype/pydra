9c16ed979f34fca0d38d0a62a961ac0b32dcf410
Dorota Jarecka <djarecka@gmail.com>
2020-06-16 13:32:10 -0400

    small fix to the bet exampel

--- pydra/engine/tests/test_shelltask.py
+++ pydra/engine/tests/test_shelltask.py
@@ -2267,7 +2267,9 @@ def test_fsl():
     )
 
     # TODO: not sure why this has to be string
-    in_file = Path(os.path.dirname(os.path.abspath(__file__))) / "data" / "foo.nii"
+    in_file = (
+        Path(os.path.dirname(os.path.abspath(__file__))) / "data_tests" / "test.nii.gz"
+    )
 
     # separate command into exec + args
     shelly = ShellCommandTask(
@@ -2275,4 +2277,4 @@ def test_fsl():
     )
     assert shelly.inputs.executable == "bet"
     assert shelly.cmdline == f"bet {in_file} {in_file}_brain"
-    # res = shelly(plugin="cf")
+    res = shelly(plugin="cf")
