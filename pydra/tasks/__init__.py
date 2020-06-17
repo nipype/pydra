08ab150293465fef3e3d115c78a2e650585df3e8
Chris Markiewicz <markiewicz@stanford.edu>
2020-06-17 06:41:16 -0400

    DOC: Add docstring, add pydra.tasks to TOC

--- pydra/tasks/__init__.py
+++ pydra/tasks/__init__.py
@@ -1,3 +1,10 @@
+""" Pydra tasks
+
+The ``pydra.tasks`` namespace is reserved for collections of Tasks, to be managed and
+packaged separately.
+To create a task package, please fork the `pydra-tasks-template
+<https://github.com/nipype/pydra-tasks-template>`__.
+"""
 try:
     __import__("pkg_resources").declare_namespace(__name__)
 except ImportError:
