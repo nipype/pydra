6a8874f34145f320ef0cb52cdd5236e6bf5c3ce0
Nicol Lo <nlo@mit.edu>
2020-10-13 04:24:00 -0400

    add tmpdir to tests in test_numpy_examples.py

--- pydra/engine/tests/test_numpy_examples.py
+++ pydra/engine/tests/test_numpy_examples.py
@@ -17,12 +17,13 @@ def arrayout(val):
     return np.array([val, val])
 
 
-def test_multiout(plugin):
+def test_multiout(plugin, tmpdir):
     """ testing a simple function that returns a numpy array"""
     wf = Workflow("wf", input_spec=["val"], val=2)
     wf.add(arrayout(name="mo", val=wf.lzin.val))
 
     wf.set_output([("array", wf.mo.lzout.b)])
+    wf.cache_dir = tmpdir
 
     with Submitter(plugin=plugin, n_procs=2) as sub:
         sub(runnable=wf)
@@ -33,13 +34,14 @@ def test_multiout(plugin):
     assert np.array_equal(results[1].output.array, np.array([2, 2]))
 
 
-def test_multiout_st(plugin):
+def test_multiout_st(plugin, tmpdir):
     """ testing a simple function that returns a numpy array, adding splitter"""
     wf = Workflow("wf", input_spec=["val"], val=[0, 1, 2])
     wf.add(arrayout(name="mo", val=wf.lzin.val))
     wf.mo.split("val").combine("val")
 
     wf.set_output([("array", wf.mo.lzout.b)])
+    wf.cache_dir = tmpdir
 
     with Submitter(plugin=plugin, n_procs=2) as sub:
         sub(runnable=wf)
