e41d519e1cb8230c6e64332b92d7ee81f724ef8c
yibeichan <yibeichen@ucsb.edu>
2022-12-16 08:40:39 -0800

    change sp string output

--- pydra/engine/tests/utils.py
+++ pydra/engine/tests/utils.py
@@ -33,7 +33,7 @@ need_gitannex = pytest.mark.skipif(
     not (shutil.which("git-annex"))
     or bool(
         float(
-            sp.check_output(["git-annex", "version", "--raw"], universal_newlines=True)
+            sp.check_output(["git-annex", "version", "--raw"], universal_newlines=True)[:6]
         )
         < 8.20200309
     ),
