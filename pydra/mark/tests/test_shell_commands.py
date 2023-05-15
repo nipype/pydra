b471f455825e24681f381f6e869f112500047db5
Tom Close <tom.g.close@gmail.com>
2023-05-15 13:20:06 +1000

    implemented shell_task and basic unittests. Generated tasks do not work as need to determine best way to map onto input_spec

--- pydra/mark/tests/test_shell_commands.py
+++ pydra/mark/tests/test_shell_commands.py
@@ -1,69 +1,187 @@
 import os
 import tempfile
-from pathlib import Path
 import attrs
-import pydra.engine
+from pathlib import Path
+import pytest
+import cloudpickle as cp
 from pydra.mark import shell_task, shell_arg, shell_out
 
 
-def test_shell_task_full():
-    @attrs.define(kw_only=True, slots=False)
-    class LsInputSpec(pydra.specs.ShellSpec):
-        directory: os.PathLike = shell_arg(
-            help_string="the directory to list the contents of",
-            argstr="",
-            mandatory=True,
-        )
-        hidden: bool = shell_arg(help_string=("display hidden FS objects"), argstr="-a")
-        long_format: bool = shell_arg(
-            help_string=(
-                "display properties of FS object, such as permissions, size and timestamps "
-            ),
-            argstr="-l",
-        )
-        human_readable: bool = shell_arg(
-            help_string="display file sizes in human readable form",
-            argstr="-h",
-            requires=["long_format"],
-        )
-        complete_date: bool = shell_arg(
-            help_string="Show complete date in long format",
-            argstr="-T",
-            requires=["long_format"],
-            xor=["date_format_str"],
-        )
-        date_format_str: str = shell_arg(
-            help_string="format string for ",
-            argstr="-D",
-            requires=["long_format"],
-            xor=["complete_date"],
+def list_entries(stdout):
+    return stdout.split("\n")[:-1]
+
+
+@pytest.fixture
+def tmpdir():
+    return Path(tempfile.mkdtemp())
+
+
+@pytest.fixture(params=["static", "dynamic"])
+def Ls(request):
+    if request.param == "static":
+
+        @shell_task
+        class Ls:
+            executable = "ls"
+
+            class Inputs:
+                directory: os.PathLike = shell_arg(
+                    help_string="the directory to list the contents of",
+                    argstr="",
+                    mandatory=True,
+                )
+                hidden: bool = shell_arg(
+                    help_string=("display hidden FS objects"),
+                    argstr="-a",
+                    default=False,
+                )
+                long_format: bool = shell_arg(
+                    help_string=(
+                        "display properties of FS object, such as permissions, size and "
+                        "timestamps "
+                    ),
+                    default=False,
+                    argstr="-l",
+                )
+                human_readable: bool = shell_arg(
+                    help_string="display file sizes in human readable form",
+                    argstr="-h",
+                    default=False,
+                    requires=["long_format"],
+                )
+                complete_date: bool = shell_arg(
+                    help_string="Show complete date in long format",
+                    argstr="-T",
+                    default=False,
+                    requires=["long_format"],
+                    xor=["date_format_str"],
+                )
+                date_format_str: str = shell_arg(
+                    help_string="format string for ",
+                    argstr="-D",
+                    default=None,
+                    requires=["long_format"],
+                    xor=["complete_date"],
+                )
+
+            class Outputs:
+                entries: list = shell_out(
+                    help_string="list of entries returned by ls command",
+                    callable=list_entries,
+                )
+
+    elif request.param == "dynamic":
+        Ls = shell_task(
+            "Ls",
+            executable="ls",
+            input_fields={
+                "directory": {
+                    "type": os.PathLike,
+                    "help_string": "the directory to list the contents of",
+                    "argstr": "",
+                    "mandatory": True,
+                },
+                "hidden": {
+                    "type": bool,
+                    "help_string": "display hidden FS objects",
+                    "argstr": "-a",
+                },
+                "long_format": {
+                    "type": bool,
+                    "help_string": (
+                        "display properties of FS object, such as permissions, size and "
+                        "timestamps "
+                    ),
+                    "argstr": "-l",
+                },
+                "human_readable": {
+                    "type": bool,
+                    "help_string": "display file sizes in human readable form",
+                    "argstr": "-h",
+                    "requires": ["long_format"],
+                },
+                "complete_date": {
+                    "type": bool,
+                    "help_string": "Show complete date in long format",
+                    "argstr": "-T",
+                    "requires": ["long_format"],
+                    "xor": ["date_format_str"],
+                },
+                "date_format_str": {
+                    "type": str,
+                    "help_string": "format string for ",
+                    "argstr": "-D",
+                    "requires": ["long_format"],
+                    "xor": ["complete_date"],
+                },
+            },
+            output_fields={
+                "entries": {
+                    "type": list,
+                    "help_string": "list of entries returned by ls command",
+                    "callable": list_entries,
+                }
+            },
         )
 
-    def list_outputs(stdout):
-        return stdout.split("\n")[:-1]
+    else:
+        assert False
 
-    @attrs.define(kw_only=True, slots=False)
-    class LsOutputSpec(pydra.specs.ShellOutSpec):
-        entries: list = shell_out(
-            help_string="list of entries returned by ls command", callable=list_outputs
-        )
+    return Ls
 
-    class Ls(pydra.engine.ShellCommandTask):
-        """Task definition for the `ls` command line tool"""
 
-        executable = "ls"
+def test_shell_task_fields(Ls):
+    assert [a.name for a in attrs.fields(Ls.Inputs)] == [
+        "executable",
+        "args",
+        "directory",
+        "hidden",
+        "long_format",
+        "human_readable",
+        "complete_date",
+        "date_format_str",
+    ]
 
-        input_spec = pydra.specs.SpecInfo(
-            name="LsInput",
-            bases=(LsInputSpec,),
-        )
+    assert [a.name for a in attrs.fields(Ls.Outputs)] == [
+        "return_code",
+        "stdout",
+        "stderr",
+        "entries",
+    ]
 
-        output_spec = pydra.specs.SpecInfo(
-            name="LsOutput",
-            bases=(LsOutputSpec,),
-        )
 
-    tmpdir = Path(tempfile.mkdtemp())
+def test_shell_task_pickle_roundtrip(Ls, tmpdir):
+    pkl_file = tmpdir / "ls.pkl"
+    with open(pkl_file, "wb") as f:
+        cp.dump(Ls, f)
+
+    with open(pkl_file, "rb") as f:
+        RereadLs = cp.load(f)
+
+    assert RereadLs is Ls
+
+
+@pytest.mark.xfail(
+    reason=(
+        "Need to change relationship between Inputs/Outputs and input_spec/output_spec "
+        "for the task to run"
+    )
+)
+def test_shell_task_init(Ls, tmpdir):
+    inputs = Ls.Inputs(directory=tmpdir)
+    assert inputs.directory == tmpdir
+    assert not inputs.hidden
+    outputs = Ls.Outputs(entries=[])
+    assert outputs.entries == []
+
+
+@pytest.mark.xfail(
+    reason=(
+        "Need to change relationship between Inputs/Outputs and input_spec/output_spec "
+        "for the task to run"
+    )
+)
+def test_shell_task_run(Ls, tmpdir):
     Path.touch(tmpdir / "a")
     Path.touch(tmpdir / "b")
     Path.touch(tmpdir / "c")
