b6e0ad7a0ab60f83869413bfad572d28b126d3c1
Tom Close <tom.g.close@gmail.com>
2023-06-14 16:13:51 +1000

    remove commented code

--- pydra/engine/tests/test_helpers_file.py
+++ pydra/engine/tests/test_helpers_file.py
@@ -1,14 +1,6 @@
-import os
-import sys
-import pytest
 from pathlib import Path
-
+import pytest
 from ..helpers_file import (
-    # split_filename,
-    # fname_presuffix,
-    # copyfile,
-    # copyfiles,
-    # get_related_files,
     ensure_list,
     MountIndentifier,
 )
@@ -16,38 +8,6 @@ from ..helpers_file import (
 
 def _ignore_atime(stat):
     return stat[:7] + stat[8:]
-
-
-# @pytest.mark.parametrize(
-#     "filename, split",
-#     [
-#         ("foo.nii", ("", "foo", ".nii")),
-#         ("foo.nii.gz", ("", "foo", ".nii.gz")),
-#         ("foo.niml.dset", ("", "foo", ".niml.dset")),
-#         ("/usr/local/foo.nii.gz", ("/usr/local", "foo", ".nii.gz")),
-#         ("../usr/local/foo.nii", ("../usr/local", "foo", ".nii")),
-#         ("/usr/local/foo.a.b.c.d", ("/usr/local", "foo.a.b.c", ".d")),
-#         ("/usr/local/", ("/usr/local", "", "")),
-#     ],
-# )
-# def test_split_filename(filename, split):
-#     res = split_filename(filename)
-#     assert res == split
-
-
-# @pytest.mark.skipif(
-#     sys.platform.startswith("win"),
-#     reason="windows drive not known in advance",
-# )
-# def test_fname_presuffix():
-#     fname = "foo.nii"
-#     pth = fname_presuffix(fname, "pre_", "_post", "/tmp")
-#     assert pth == str(Path("/tmp/pre_foo_post.nii"))
-#     fname += ".gz"
-#     pth = fname_presuffix(fname, "pre_", "_post", "/tmp")
-#     assert pth == str(Path("/tmp/pre_foo_post.nii.gz"))
-#     pth = fname_presuffix(fname, "pre_", "_post", "/tmp", use_ext=False)
-#     assert pth == str(Path("/tmp/pre_foo_post"))
 
 
 @pytest.fixture()
@@ -70,148 +30,6 @@ def _temp_analyze_files_prime(tmpdir):
     return Path(orig_img.strpath), Path(orig_hdr.strpath)
 
 
-# def test_copyfile(_temp_analyze_files):
-#     orig_img, orig_hdr = _temp_analyze_files
-#     pth, fname = os.path.split(orig_img)
-#     new_img = os.path.join(pth, "newfile.img")
-#     new_hdr = os.path.join(pth, "newfile.hdr")
-#     copyfile(orig_img, new_img)
-#     assert os.path.exists(new_img)
-#     assert os.path.exists(new_hdr)
-
-
-# def test_copyfile_true(_temp_analyze_files):
-#     orig_img, orig_hdr = _temp_analyze_files
-#     pth, fname = os.path.split(orig_img)
-#     new_img = os.path.join(pth, "newfile.img")
-#     new_hdr = os.path.join(pth, "newfile.hdr")
-#     # Test with copy=True
-#     copyfile(orig_img, new_img, copy=True)
-#     assert os.path.exists(new_img)
-#     assert os.path.exists(new_hdr)
-
-
-# def test_copyfiles(_temp_analyze_files, _temp_analyze_files_prime):
-#     orig_img1, orig_hdr1 = _temp_analyze_files
-#     orig_img2, orig_hdr2 = _temp_analyze_files_prime
-#     pth, fname = os.path.split(orig_img1)
-#     new_img1 = os.path.join(pth, "newfile.img")
-#     new_hdr1 = os.path.join(pth, "newfile.hdr")
-#     pth, fname = os.path.split(orig_img2)
-#     new_img2 = os.path.join(pth, "secondfile.img")
-#     new_hdr2 = os.path.join(pth, "secondfile.hdr")
-#     # providing specific filenames for a new destinations
-#     copyfiles([orig_img1, orig_img2], [new_img1, new_img2])
-#     # checking if the new files exist (together with hdr files)
-#     assert os.path.exists(new_img1)
-#     assert os.path.exists(new_hdr1)
-#     assert os.path.exists(new_img2)
-#     assert os.path.exists(new_hdr2)
-
-
-# def test_copyfiles_destdir(_temp_analyze_files, _temp_analyze_files_prime, tmpdir):
-#     orig_img1, _ = _temp_analyze_files
-#     orig_img2, _ = _temp_analyze_files_prime
-#     _, fname = os.path.split(orig_img1)
-#     new_img1 = tmpdir.join(fname)
-#     _, fname = os.path.split(orig_img2)
-#     new_img2 = tmpdir.join(fname)
-#     # providing directory as a new destination
-#     copyfiles([orig_img1, orig_img2], tmpdir)
-#     assert os.path.exists(new_img1)
-#     assert os.path.exists(new_img2)
-
-
-# def test_linkchain(_temp_analyze_files):
-#     if os.name != "posix":
-#         return
-#     orig_img, orig_hdr = _temp_analyze_files
-#     pth, fname = os.path.split(orig_img)
-#     new_img1 = os.path.join(pth, "newfile1.img")
-#     new_hdr1 = os.path.join(pth, "newfile1.hdr")
-#     new_img2 = os.path.join(pth, "newfile2.img")
-#     new_hdr2 = os.path.join(pth, "newfile2.hdr")
-#     new_img3 = os.path.join(pth, "newfile3.img")
-#     new_hdr3 = os.path.join(pth, "newfile3.hdr")
-#     copyfile(orig_img, new_img1, use_hardlink=False)
-#     assert os.path.islink(new_img1)
-#     assert os.path.islink(new_hdr1)
-#     copyfile(new_img1, new_img2, copy=True, use_hardlink=False)
-#     assert not os.path.islink(new_img2)
-#     assert not os.path.islink(new_hdr2)
-#     assert not os.path.samefile(orig_img, new_img2)
-#     assert not os.path.samefile(orig_hdr, new_hdr2)
-#     copyfile(new_img1, new_img3, copy=True, use_hardlink=True)
-#     assert not os.path.islink(new_img3)
-#     assert not os.path.islink(new_hdr3)
-#     assert os.path.samefile(orig_img, new_img3)
-#     assert os.path.samefile(orig_hdr, new_hdr3)
-
-
-# def test_recopy(_temp_analyze_files):
-#     # Re-copying with the same parameters on an unchanged file should be
-#     # idempotent
-#     #
-#     # Test for copying from regular files and symlinks
-#     orig_img, orig_hdr = _temp_analyze_files
-#     pth, fname = os.path.split(orig_img)
-#     img_link = os.path.join(pth, "imglink.img")
-#     new_img = os.path.join(pth, "newfile.img")
-#     new_hdr = os.path.join(pth, "newfile.hdr")
-#     copyfile(orig_img, img_link)
-#     for copy in (True, False):
-#         for use_hardlink in (True, False):
-#             kwargs = {"copy": copy, "use_hardlink": use_hardlink}
-
-#             copyfile(orig_img, new_img, **kwargs)
-#             img_stat = _ignore_atime(os.stat(new_img))
-#             hdr_stat = _ignore_atime(os.stat(new_hdr))
-#             copyfile(orig_img, new_img, **kwargs)
-#             err_msg = "Regular - OS: {}; Copy: {}; Hardlink: {}".format(
-#                 os.name, copy, use_hardlink
-#             )
-#             assert img_stat == _ignore_atime(os.stat(new_img)), err_msg
-#             assert hdr_stat == _ignore_atime(os.stat(new_hdr)), err_msg
-#             os.unlink(new_img)
-#             os.unlink(new_hdr)
-
-#             copyfile(img_link, new_img, **kwargs)
-#             img_stat = _ignore_atime(os.stat(new_img))
-#             hdr_stat = _ignore_atime(os.stat(new_hdr))
-#             copyfile(img_link, new_img, **kwargs)
-#             err_msg = "Symlink - OS: {}; Copy: {}; Hardlink: {}".format(
-#                 os.name, copy, use_hardlink
-#             )
-#             assert img_stat == _ignore_atime(os.stat(new_img)), err_msg
-#             assert hdr_stat == _ignore_atime(os.stat(new_hdr)), err_msg
-#             os.unlink(new_img)
-#             os.unlink(new_hdr)
-
-
-# def test_get_related_files(_temp_analyze_files):
-#     orig_img, orig_hdr = _temp_analyze_files
-
-#     related_files = get_related_files(orig_img)
-#     assert orig_img in related_files
-#     assert orig_hdr in related_files
-
-#     related_files = get_related_files(orig_hdr)
-#     assert orig_img in related_files
-#     assert orig_hdr in related_files
-
-
-# def test_get_related_files_noninclusive(_temp_analyze_files):
-#     orig_img, orig_hdr = _temp_analyze_files
-
-#     related_files = get_related_files(orig_img, include_this_file=False)
-#     assert orig_img not in related_files
-#     assert orig_hdr in related_files
-
-#     related_files = get_related_files(orig_hdr, include_this_file=False)
-#     assert orig_img in related_files
-#     assert orig_hdr not in related_files
-
-
 @pytest.mark.parametrize(
     "filename, expected",
     [
@@ -224,33 +42,6 @@ def _temp_analyze_files_prime(tmpdir):
 def test_ensure_list(filename, expected):
     x = ensure_list(filename)
     assert x == expected
-
-
-# @pytest.mark.parametrize(
-#     "file, length, expected_files",
-#     [
-#         (
-#             "/path/test.img",
-#             3,
-#             [Path("/path/test.hdr"), Path("/path/test.img"), Path("/path/test.mat")],
-#         ),
-#         (
-#             "/path/test.hdr",
-#             3,
-#             [Path("/path/test.hdr"), Path("/path/test.img"), Path("/path/test.mat")],
-#         ),
-#         ("/path/test.BRIK", 2, [Path("/path/test.BRIK"), Path("/path/test.HEAD")]),
-#         ("/path/test.HEAD", 2, [Path("/path/test.BRIK"), Path("/path/test.HEAD")]),
-#         ("/path/foo.nii", 2, [Path("/path/foo.nii"), Path("/path/foo.mat")]),
-#     ],
-# )
-# def test_related_files(file, length, expected_files):
-#     related_files = get_related_files(file)
-
-#     assert len(related_files) == length
-
-#     for ef in expected_files:
-#         assert ef in related_files
 
 
 MOUNT_OUTPUTS = (
