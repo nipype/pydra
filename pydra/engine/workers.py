fe97f8e79e0f8337663a56e8f958467bfd2881b2
Aditya Agarwal <50960175+adi611@users.noreply.github.com>
2023-09-20 14:15:58 +0530

    fix: could not raise exception

--- pydra/engine/workers.py
+++ pydra/engine/workers.py
@@ -911,9 +911,7 @@ class PsijWorker(Worker):
         spec = self.psij.JobSpec()
         spec.executable = cmd
         spec.arguments = arg
-        spec.stdout_path = "demo.stdout"
-        spec.stderr_path = "demo.stderr"
-
+        
         return spec
 
     def make_job(self, spec, attributes):
@@ -960,10 +958,18 @@ class PsijWorker(Worker):
                 ],
             )
 
+        spec.stdout_path = os.path.join(cache_dir, "demo.stdout")
+        spec.stderr_path = os.path.join(cache_dir, "demo.stderr")
+        
         job = self.make_job(spec, None)
         jex.submit(job)
         job.wait()
 
+        if os.path.getsize(spec.stderr_path) > 0:
+            with open(spec.stderr_path, "r") as stderr_file:
+                stderr_contents = stderr_file.read()
+            raise Exception(f"stderr_path '{spec.stderr_path}' is not empty. Contents:\n{stderr_contents}")
+        
         return
 
     def close(self):
