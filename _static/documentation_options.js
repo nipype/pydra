abc3a18c1a285920d15b564b8199b3228eea6148
tclose <tclose@users.noreply.github.com>
2025-03-19 02:59:13 +0000

    deploy: 60c7856fc20331417da4ae15f8cea2a82ca29c7b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.25.dev387+gf20fc232',
+    VERSION: '0.25.dev389+g60c7856f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
