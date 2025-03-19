60e4bf87cab927938469e61b32ab4e6d327dbfcd
Tom Close <tom.g.close@gmail.com>
2025-03-20 09:38:40 +1100

    added arg to run-docker-slurm tests

--- docker-scripts-for-tests/run-docker-slurm-test.sh
+++ docker-scripts-for-tests/run-docker-slurm-test.sh
@@ -1,1 +1,8 @@
-docker exec slurm bash -c "pytest /pydra/pydra --color=yes -vs -k 'not test_audit_prov and not test_audit_prov_messdir_1 and not test_audit_prov_messdir_2 and not test_audit_prov_wf and not test_audit_all'"
+if [ -z "$1" ]; then
+    TEST="pydra"
+else
+    TEST=$1
+fi
+
+
+docker exec slurm bash -c "pytest /pydra/$TEST --color=yes -vs -k 'not test_audit_prov and not test_audit_prov_messdir_1 and not test_audit_prov_messdir_2 and not test_audit_prov_wf and not test_audit_all'"
