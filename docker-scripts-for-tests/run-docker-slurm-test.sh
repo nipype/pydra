37356b4e2329ceddcd8ec8b0e43cc82e67fd17bf
Tom Close <tom.g.close@gmail.com>
2025-03-20 11:32:35 +1100

    added docker scripts to help debug slurm worker

--- docker-scripts-for-tests/run-docker-slurm-test.sh
+++ docker-scripts-for-tests/run-docker-slurm-test.sh
@@ -5,4 +5,4 @@ else
 fi
 
 
-docker exec slurm bash -c "pytest -vv /pydra/$TEST --color=yes -vs -k 'not test_audit_prov and not test_audit_prov_messdir_1 and not test_audit_prov_messdir_2 and not test_audit_prov_wf and not test_audit_all' $2"
+docker exec pydra-slurm-docker bash -c "pytest -vv -s /pydra/$TEST --color=yes -vs -k 'not test_audit_prov and not test_audit_prov_messdir_1 and not test_audit_prov_messdir_2 and not test_audit_prov_wf and not test_audit_all' $2"
--- docker-scripts-for-tests/start-docker-slurm.sh
+++ docker-scripts-for-tests/start-docker-slurm.sh
@@ -1,27 +1,23 @@
 PKG_DIR=$(realpath $(dirname $0)/..)
-DOCKER_IMAGE=adi611/docker-centos7-slurm:23.02.1
-PYTHON_VERSION=3.11.5
 NO_ET=TRUE
 
 # Pull image
-docker pull $DOCKER_IMAGE
+docker build -t pydra-slurm-docker $PKG_DIR/docker-scripts-for-tests
 
 # Start image
-docker run --rm -itd -h slurmctl --cap-add sys_admin -d --name slurm -v $PKG_DIR:/pydra -e NO_ET=$NO_ET $DOCKER_IMAGE
+docker run --rm -itd -h slurmctl --cap-add sys_admin -d --name pydra-slurm-docker -v $PKG_DIR:/pydra -e NO_ET=$NO_ET pydra-slurm-docker
 
 # Display previous jobs with sacct
-echo "Allowing ports/daemons time to start" && sleep 10
-docker exec slurm bash -c "sacctmgr -i add account none,test Cluster=linux Description='none' Organization='none'"
-docker exec slurm bash -c "sacct && sinfo && squeue" 2&> /dev/null
+echo "Allowing ports/daemons time to start" && sleep 20
+docker exec pydra-slurm-docker bash -c "sacctmgr -i add account none,test Cluster=linux Description='none' Organization='none'"
+docker exec pydra-slurm-docker bash -c "sacct && sinfo && squeue" 2&> /dev/null
 if [ $? -ne 0 ]; then
     echo "Slurm docker image error"
     exit 1
 fi
 
 # Setup Python
-docker exec slurm bash -c "echo $NO_ET"
-docker exec slurm bash -c "ls -la && echo list top level dir"
-docker exec slurm bash -c "ls -la /pydra && echo list pydra dir"
-docker exec slurm bash -c "CONFIGURE_OPTS=\"-with-openssl=/opt/openssl\" pyenv install -v ${PYTHON_VERSION}"
-docker exec slurm bash -c "pyenv global ${PYTHON_VERSION}"
-docker exec slurm bash -c "pip install --upgrade pip && pip install -e /pydra[test,psij] && python -c 'import pydra.engine; print(pydra.engine.__version__)'"
+docker exec pydra-slurm-docker bash -c "echo $NO_ET"
+docker exec pydra-slurm-docker bash -c "ls -la && echo list top level dir"
+docker exec pydra-slurm-docker bash -c "ls -la /pydra && echo list pydra dir"
+docker exec pydra-slurm-docker bash -c "pip install --upgrade pip && pip install -e /pydra[test,psij] && python -c 'import pydra.engine; print(pydra.engine.__version__)'"
