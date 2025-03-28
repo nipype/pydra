ab5bf220c0d49ca1fef2fc800788e3dc7435dfeb
Tom Close <tom.g.close@gmail.com>
2025-03-28 20:27:08 +1100

    fixed up import locations in tutorials

--- pydra/utils/__init__.py
+++ pydra/utils/__init__.py
@@ -1,8 +1,9 @@
 from .general import (
     task_fields,
+    fields_dict,
     plot_workflow,
     print_help,
 )
 from ._version import __version__
 
-__all__ = ["__version__", "task_fields", "plot_workflow", "print_help"]
+__all__ = ["__version__", "task_fields", "plot_workflow", "print_help", "fields_dict"]
