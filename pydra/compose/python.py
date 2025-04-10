6b54a295f51004e67d59dbca5442c98eab2b1f63
Tom Close <tom.g.close@gmail.com>
2025-04-10 10:55:27 +1000

    renamed task_dict to task_as_dict and added to pydra.utils.__init__

--- pydra/compose/python.py
+++ pydra/compose/python.py
@@ -2,7 +2,7 @@ import typing as ty
 import inspect
 from typing import dataclass_transform
 import attrs
-from pydra.utils.general import task_fields, task_dict
+from pydra.utils.general import task_fields, task_as_dict
 from pydra.compose import base
 from pydra.compose.base import (
     ensure_field_objects,
@@ -236,7 +236,7 @@ class PythonTask(base.Task[PythonOutputsType]):
 
     def _run(self, job: "Job[PythonTask]", rerun: bool = True) -> None:
         # Prepare the inputs to the function
-        inputs = task_dict(self)
+        inputs = task_as_dict(self)
         del inputs["function"]
         # Run the actual function
         returned = self.function(**inputs)
--- pydra/utils/__init__.py
+++ pydra/utils/__init__.py
@@ -1,5 +1,6 @@
 from .general import (
     task_fields,
+    task_as_dict,
     plot_workflow,
     show_workflow,
     task_help,
@@ -12,6 +13,7 @@ from ._version import __version__
 __all__ = [
     "__version__",
     "task_fields",
+    "task_as_dict",
     "plot_workflow",
     "show_workflow",
     "task_help",
